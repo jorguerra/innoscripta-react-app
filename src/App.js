5d97633e6eeb28ff81544137f991278ebc7b2e53
Jorge Guerra <jorguerra@gmail.com>
2019-12-03 00:26:41 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header'
+import {BrowserRouter as Router} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axios = require('axios').default
+
+export default class App extends Component {
+  state = { user: { id: null, admin:false }, pizzas: {}, order: [] }
+  
+  constructor(props){
+    super(props);
+    this.token = document.getElementById('home').dataset.token;
+    this.api_url=props.api_url
+  }
+
+  componentDidMount(){
+    this.getUser()
+    axios.get(`${this.api_url}/pizzas`).then((resp) => this.setState({pizzas: resp.data}))
+  }
+
+  getUser = async () => {
+    if(this.token)
+      axios.get(`${this.api_url}/user?api_token=${this.token}`).then((resp) => this.setState({user: resp.data}))
+  }
+
+
+
+  render() {
+    const {id, admin} = this.state.user
+    return (
+        <Router>
+            <Header user={id} admin={admin} cart={this.state.order} />
+
+        </Router>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+const axios = require('axios').default;
+
+export default class Header extends Component {
+
+    logout(){
+        axios.post('/logout').then(() => {
+            document.location.href = '/';
+        })
+    }
+
+    render() {
+        const num_cart = this.props.cart.reduce((acc, pizza) => {
+            if (!pizza) return acc;
+            return acc + pizza.quantity
+        }
+        ,0);
+        let txt_cart = 'No pizzas yet';
+        if(num_cart)
+            txt_cart = `${num_cart} pizza` + (num_cart > 1 ? 's':'')
+        
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+                <div className="container">
+                    <Link className="navbar-brand" to="/"><span className="flaticon-pizza-1 mr-1"></span>Pizza<br/><small>Delicous</small></Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="oi oi-menu"></span> Menu
+                    </button>
+                    <div className="collapse navbar-collapse" id="ftco-nav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active"><Link to="/" className="nav-link">Home</Link></li>
+                            <li className="nav-item">
+                                <Link to="/cart" id="cart" className="nav-link">Cart <small>({txt_cart})</small></Link>
+                            </li>
+                            {this.props.user ? <li className="nav-item"><Link to="/review-orders" className="nav-link">Review orders</Link></li> : ''}
+                            {this.props.admin ? <li className="nav-item"><Link to="/manage-pizzas" className="nav-link">Manage pizzas</Link></li> : ''}
+                            <li className="nav-item">
+                                <a href={null} className="nav-link" style={{cursor: 'pointer'}} 
+                                onClick={() => this.props.user ? this.logout() : this.login(this.props.cart)}>
+                                    {this.props.user ? 'Logout' : 'Login'}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
